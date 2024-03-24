65fa056086289ac5b3e12cb27a531393f1cee7f0
MajorX234 <majorx234@googlemail.com>
2024-03-25 00:00:24 +0100

    Read arguments and check amount

--- convert_single_playlist.py
+++ convert_single_playlist.py
@@ -1,6 +1,12 @@
 import sys
 
 def main():
-    print(sys.argv)
+    if not (len(sys.argv) == 3):
+        print(sys.argv)
+        print("Need 2 arguments: source file, target file")
+        exit
+    else:    
+        source_file = sys.argv[1] 
+        target_file = sys.argv[2] 
 if __name__ == "__main__":
     main()
