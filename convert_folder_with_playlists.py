9e0b28f193536c45377a5d6edeb35e7ee50e2f88
MajorX234 <majorx234@googlemail.com>
2024-03-26 00:16:41 +0100

    Add glue code to pass files of walkdir into convert function

--- convert_folder_with_playlists.py
+++ convert_folder_with_playlists.py
@@ -2,32 +2,38 @@ import sys
 from pathlib import Path
 import re
 import os
+import codecs
 
 def main():
-    if not (len(sys.argv) == 3):
+    if not (len(sys.argv) == 4):
         print(sys.argv)
-        print("Need 2 arguments: directory and prefix path")
+        print("Need 2 arguments: source dir, target_dir and prefix path")
         exit
-    #source_file = Path(sys.argv[1])
-    #target_file = Path(sys.argv[2])
-    #prefix_path = sys.argv[3]
-    files = walkdir(Path(sys.argv[1]))
-    print(files)
+    source_folder = Path(sys.argv[1])
+    target_folder = Path(sys.argv[2])
+    prefix_path = sys.argv[3]
+    files = walkdir(source_folder)
+    for file in files:
+        source_file = source_folder / file
+        target_file = target_folder / file
+        print(source_file)
+        if not target_file.parent.exists():
+            target_file.parent.mkdir(parents=True)
+        convert_file(source_file, target_file, prefix_path)
 
 def walkdir(dirname: Path) -> [Path]:
     walk_files = []
     for cur, _dirs, files in os.walk(dirname):
-        print(cur)
         for f in files:
             path = Path(cur + "/" + f)
-            walk_files.append(path)
+            walk_files.append(path.relative_to(dirname))
     return walk_files
 
-def convert_file(source_file: Path, target_file: Path, prefix: str):
+def convert_file(source_file: Path, target_file: Path, prefix_path: str):
     if not source_file.exists():
         print("source file error: not exist")
-    with source_file.open('r') as sfile:
-        with target_file.open('a') as tfile:
+    with source_file.open('r', encoding="ISO-8859-1") as sfile:
+        with target_file.open('a', encoding="ISO-8859-1") as tfile:
             lines = sfile.readlines()
             for line in lines:
                 new_line = line.replace("E:\\", prefix_path).replace("\\", "/")
